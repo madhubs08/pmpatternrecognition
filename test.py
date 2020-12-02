78b200875fa0d94bfb4466c3acbdd354cb3051de
Madhavi <madhubs08@gmail.com>
2020-12-02 11:03:37 +0100

    Revert "First commit"
    This reverts commit 87f2b0ddd4d394b9e622698bee7ac5e9acf1cd5f.

--- test.py
+++
@@ -1,5 +0,0 @@
-pattern_list = [{'ID':'0', 'Name': 'apg', 'Pattern': ['Add penalty', 'Payment']},
-                            {'ID':'1', 'Name': 'cpg', 'Pattern': ['Create Fine', 'Payment']}]
-
-ids_user_patterns = [x['ID'] for x in pattern_list]
-print("ids_user_patterns = ",ids_user_patterns)
